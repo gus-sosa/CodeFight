4f797efc0e20fe39e914b246c150390bcf5b2844
Gustavo Sosa <g.sosacuervo1@gmail.com>
2017-11-13 09:24:52 -0600

    Trying a solution to the problem First non repeating character
    https://codefights.com/interview-practice/task/uX5iLwhc6L5ckSyNC

---
+++ firstNonRepeatingCharacter/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstNonRepeatingCharacter
+{
+    class Program
+    {
+        char firstNotRepeatingCharacter(string s)
+        {
+            int firstIndex = -1;
+            char firstNonRepeatingCharacter = '_';
+            bool[] map = new bool[26];
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char currentCharacter = s[i];
+                int indexInMap = currentCharacter - 'a';
+                if (map[indexInMap])
+                {
+                    
+                }
+                else
+                {
+                        
+                }
+            }
+
+            return firstNonRepeatingCharacter;
+        }
+
+
+        static void Main(string[] args)
+        {
+            var p = new Program();
+            Console.WriteLine(p.firstNotRepeatingCharacter("a"));
+        }
+    }
+}
