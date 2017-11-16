881a0e4322a6613ae6207265f04a7afe7d815be5
Gustavo Sosa <g.sosacuervo1@gmail.com>
2017-11-16 10:21:24 -0600

    First solution
    It is wrong. It is not deleting the first element if it matches an element in the first position

--- RemoveKFromList/Program.cs
+++ RemoveKFromList/Program.cs
@@ -43,18 +43,22 @@ namespace RemoveKFromList
 
         private static void PrintList(ListNode<int> listNode)
         {
-            throw new NotImplementedException();
+            var current = listNode;
+            while (current != null)
+            {
+                Console.Write($"{current.value} ");
+                current = current.next;
+            }
+            Console.WriteLine();
+            Console.WriteLine();
         }
 
         private static ListNode<int> CreateList(int[] nums)
         {
-            var list=new ListNode<int>();
-            var current = list;
-            foreach (int num in nums)
-            {
-                current.value = num;
-                
-            }
+            var list = nums.Select(i => new ListNode<int>() { value = i }).ToArray();
+            for (int i = 0; i < list.Length - 1; i++)
+                list[i].next = list[i + 1];
+            return list[0];
         }
     }
 }
