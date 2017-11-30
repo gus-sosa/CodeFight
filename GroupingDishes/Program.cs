b931f1ecf5c76d8ca71a64b70902e4b612c4d8b5
Gustavo Sosa <g.sosacuervo1@gmail.com>
2017-11-30 00:06:56 -0600

    Partial solution

--- GroupingDishes/Program.cs
+++ GroupingDishes/Program.cs
@@ -8,6 +8,41 @@ namespace GroupingDishes
 {
     class Program
     {
+        string[][] groupingDishes(string[][] dishes)
+        {
+            var hash = new Dictionary<string, List<string>>();
+            foreach (string[] dish in dishes)
+            {
+                var nameDish = dish[0];
+                var ingredients = dish.Skip(1);
+                foreach (var ingredient in ingredients)
+                {
+                    if (!hash.ContainsKey(ingredient))
+                        hash[ingredient] = new List<string>();
+                    hash[ingredient].Add(nameDish);
+                }
+            }
+
+            var list = new List<string[]>();
+
+            foreach (string hashKey in hash.Keys)
+            {
+                hash[hashKey].Sort();
+                list.Add(new[] { hashKey }.Concat(hash[hashKey].ToArray()).ToArray());
+            }
+
+            list.Sort(new DishComparison());
+            return list.ToArray();
+        }
+
+        class DishComparison : IComparer<string[]>
+        {
+            public int Compare(string[] x, string[] y)
+            {
+                return String.CompareOrdinal(x[0], y[0]);
+            }
+        }
+
         static void Main(string[] args)
         {
         }
