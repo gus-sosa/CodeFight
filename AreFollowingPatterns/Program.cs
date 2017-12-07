2f0726574c93151497f6004aaf2916ce543e5a79
Gustavo Sosa <g.sosacuervo1@gmail.com>
2017-12-07 10:43:21 -0600

    Project created

---
+++ AreFollowingPatterns/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreFollowingPatterns
+{
+    class Program
+    {
+        bool areFollowingPatterns(string[] strings, string[] patterns)
+        {
+
+        }
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
