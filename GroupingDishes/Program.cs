67b554edd684e6490c988f65eb0266392a8b6f70
Gustavo Sosa <g.sosacuervo1@gmail.com>
2017-12-07 10:40:59 -0600

    Solution to problem Grouping Dishes
    https://codefights.com/interview-practice/task/xrFgR63cw7Nch4vXo

--- GroupingDishes/Program.cs
+++ GroupingDishes/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GroupingDishes
 {
@@ -25,18 +24,27 @@ namespace GroupingDishes
 
             var list = new List<string[]>();
 
+            var dishComparer = new DishComparison();
             foreach (string hashKey in hash.Keys)
-            {
-                hash[hashKey].Sort();
                 if (hash[hashKey].Count >= 2)
+                {
+                    hash[hashKey].Sort(dishComparer);
                     list.Add(new[] { hashKey }.Concat(hash[hashKey].ToArray()).ToArray());
-            }
+                }
 
-            list.Sort(new DishComparison());
+            list.Sort(new IngredientComparison());
             return list.ToArray();
         }
 
-        class DishComparison : IComparer<string[]>
+        class DishComparison : IComparer<string>
+        {
+            public int Compare(string x, string y)
+            {
+                return StringComparer.Ordinal.Compare(x, y);
+            }
+        }
+
+        class IngredientComparison : IComparer<string[]>
         {
             public int Compare(string[] x, string[] y)
             {
@@ -48,25 +56,28 @@ namespace GroupingDishes
         {
             var p = new Program();
             var result = p.groupingDishes(new[]
-            {
-                new[] {"dSaLKJGbxlxcBBv kMNOmzdojCluYeCb","zjxwKcRmpQTPSPRUKLfAhkIXxfzniZjsDfaKOJOcVDaxAnCF"},
-                new[] {"R GBgXIv","fPhNHIdOTeKPnqaIPAYXScGrDyGWwlqktYtyOzondayKp","xJ hfufIWL","YduFVZrZEeqVmvACdSTdQd uMswBcadvet","WHYOLUzwSHKUuCNry"},
-                new[] {"kvHxWutzASOCBHV","wpzmQKLROsw ","sxgFkhrwFKB","reRqP TFlpmiQa GoZTuErWVB","LA","BGQgMdEGXutmmE InKtapSHbwZlPHrvPwbSmRWclamnTW","QgBClPTxsIpAl","SWbXtEIFeVqoUgtSfXKcVmnwDrijLYsPeXfUauFVbBkbEmGDa"} ,
+            {new[]{"dSaLKJGbxlxcBBv kMNOmzdojCluYeCb","zjxwKcRmpQTPSPRUKLfAhkIXxfzniZjsDfaKOJOcVDaxAnCF"},
+                new[]{"R GBgXIv","fPhNHIdOTeKPnqaIPAYXScGrDyGWwlqktYtyOzondayKp","xJ hfufIWL","YduFVZrZEeqVmvACdSTdQd uMswBcadvet","WHYOLUzwSHKUuCNry"},
+                new[]{"kvHxWutzASOCBHV","wpzmQKLROsw ","sxgFkhrwFKB","reRqP TFlpmiQa GoZTuErWVB","LA","BGQgMdEGXutmmE InKtapSHbwZlPHrvPwbSmRWclamnTW","QgBClPTxsIpAl","SWbXtEIFeVqoUgtSfXKcVmnwDrijLYsPeXfUauFVbBkbEmGDa"},
                 new[]{"Hgpu","fvORUPNvHmBtpKpbTRbmdXzicYOZotLmfoLmQIqBInPnbCFN","WHYOLUzwSHKUuCNry"},
                 new[]{"gZxWYomyYO","fvORUPNvHmBtpKpbTRbmdXzicYOZotLmfoLmQIqBInPnbCFN","YduFVZrZEeqVmvACdSTdQd uMswBcadvet","XxRAIFwrGmaarKfz","yJffViKwbqCATxHcQFDLgMX","poEnqRtrANh","QgBClPTxsIpAl","dyqdvHDdflvzxVAGRyxWPMBtIDJhv paNyJbWab"},
                 new[]{"rMSYkYkFKlcdBTrUpCTdFgEIdgdTOcEucJdPqiLUWUZNjcoL","YduFVZrZEeqVmvACdSTdQd uMswBcadvet","QgBClPTxsIpAl","fPhNHIdOTeKPnqaIPAYXScGrDyGWwlqktYtyOzondayKp","udzzsbLVValZOWpRLhUKumkROw","dyqdvHDdflvzxVAGRyxWPMBtIDJhv paNyJbWab","WHYOLUzwSHKUuCNry","LA","fvORUPNvHmBtpKpbTRbmdXzicYOZotLmfoLmQIqBInPnbCFN"},
                 new[]{"GrWh ROg zHXhYguurdcGjNAv","dyqdvHDdflvzxVAGRyxWPMBtIDJhv paNyJbWab","YduFVZrZEeqVmvACdSTdQd uMswBcadvet","AQglifKDgZIivthzfoWRklaKs","UcIBqQckdEJgLeWMlaRPlqfkhVRXjtZHAYDVRhPsFqPOuEVN","LA","MWhqbkFrSTnOuWKexjPewdd AOISBnSCilJ","QgBClPTxsIpAl"},
                 new[]{"dLuvsltPzUjfXkynBCMgxBUTXhVCd","udzzsbLVValZOWpRLhUKumkROw","BGQgMdEGXutmmE InKtapSHbwZlPHrvPwbSmRWclamnTW","BjRRCVKznaySRzyAuNxAbkSYTmcUGlvOND","wpzmQKLROsw ","qLKOIfeBowxWwxPJWrWjtVXMkG NXOLxYxvCKoAagSHYRxVgK","WdfleYASWwVMQKoBINhwpjDbVBEaOOogkKMZ","AQglifKDgZIivthzfoWRklaKs","qRUsCllaFzNWuXIMvbOsNtTTAlbR"},
                 new[]{"jOubIROdYWOKxwbZTLDueBiln","fTUBneoUSWxFERZjwPMrAQq","NPuomEOeOXBiozrNZXlXcKKB","ibogPWJoEbermlJfuizYaE","zpNFvjef mpEbEqy rdudPTGpzo n FwxTA"},
-                new[]{"BiNYUHMFrRoSICZ","ufYAxvBidQjinsDCurHyjlzRHrOQ MbIVKGLwAq","nvHaaRJdHgRIgXXQteAchX MKldBbM","TdBMoUrYiEcJPlERejkAQdxYMTatLYXX","qLKOIfeBowxWwxPJWrWjtVXMkG NXOLxYxvCKoAagSHYRxVgK"}
-            });
+                new[]{"BiNYUHMFrRoSICZ","ufYAxvBidQjinsDCurHyjlzRHrOQ MbIVKGLwAq","nvHaaRJdHgRIgXXQteAchX MKldBbM","TdBMoUrYiEcJPlERejkAQdxYMTatLYXX","qLKOIfeBowxWwxPJWrWjtVXMkG NXOLxYxvCKoAagSHYRxVgK"}});
             PrintResult(result);
         }
 
         private static void PrintResult(string[][] result)
         {
-            foreach (var row in result)
-                Console.WriteLine(row.Aggregate("", (acc, curr) => $"{acc},{curr}").Remove(0, 1));
+            Console.Write("[");
+            for (var i = 0; i < result.Length; i++)
+            {
+                var row = result[i];
+                Console.WriteLine($"[{row.Aggregate("", (acc, curr) => $"{acc},{curr}").Remove(0, 1)}]{(i == result.Length - 1 ? "" : ",")}");
+            }
+            Console.Write("]");
         }
     }
 }
