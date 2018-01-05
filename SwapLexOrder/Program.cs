10fa4408a8bfe1a9654b2b892e967fd2a97db9a0
Gustavo Sosa <g.sosacuervo1@gmail.com>
2018-01-05 10:25:00 -0600

    Project created for this problem

---
+++ SwapLexOrder/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapLexOrder
+{
+    class Program
+    {
+        string swapLexOrder(string str, int[][] pairs)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
