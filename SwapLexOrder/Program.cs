8de29456b8d6d7bc16eb9d6c8541ca577d55bceb
Gustavo Sosa <g.sosacuervo1@gmail.com>
2018-01-12 13:37:04 -0600

    Another temporal solution

--- SwapLexOrder/Program.cs
+++ SwapLexOrder/Program.cs
@@ -53,22 +53,28 @@ namespace SwapLexOrder
 
             if (parent1 == pos1)
             {
-                set[pos1] = parent2;
                 AddToDict(dict, set, pos2, pos1);
+                set[pos1] = parent2;
             }
-            else if (parent2 == pos2)
+            else
             {
-                set[pos2] = parent1;
                 AddToDict(dict, set, pos1, pos2);
+                set[pos2] = parent1;
             }
         }
 
         private void AddToDict(Dictionary<int, SortedSet<int>> dict, int[] set, int pos1, int pos2)
         {
-            int parent = GetSet(pos1, set);
-            if (!dict.ContainsKey(parent))
-                dict[parent] = new SortedSet<int>() { pos1 };
-            dict[parent].Add(pos2);
+            int parent1 = GetSet(pos1, set);
+            int parent2 = GetSet(pos2, set);
+            if (!dict.ContainsKey(parent1))
+                dict[parent1] = new SortedSet<int>() { pos1 };
+            dict[parent1].Add(pos2);
+            if (dict.ContainsKey(parent2))
+            {
+                dict[parent1].UnionWith(dict[parent2]);
+                dict.Remove(parent2);
+            }
         }
 
         private int GetSet(int pos, int[] set)
@@ -82,10 +88,58 @@ namespace SwapLexOrder
         static void Main(string[] args)
         {
             var p = new Program();
-            Console.WriteLine(p.swapLexOrder("abdc", new[]
+            Console.WriteLine(p.swapLexOrder("wdbmjpxusweoaxfybkikectlgvrxyracjxeghyctvvexpoxibunjvswhuwduirybhrfvcybuaisbujcngdiaotysffkxocnajloq", new[]
             {
-                new[]{1, 4},
-                new[]{ 3, 4}
+                new[]{60,65},
+                new[]{41,93},
+                new[]{41,58},
+                new[]{87,92},
+                new[]{34,87},
+                new[]{37,56},
+                new[]{35,79},
+                new[]{10,54},
+                new[]{35,73},
+                new[]{56,57},
+                new[]{5,65},
+                new[]{69,91},
+                new[]{6,65},
+                new[]{72,95},
+                new[]{8,49},
+                new[]{35,62},
+                new[]{26,73},
+                new[]{38,58},
+                new[]{14,88},
+                new[]{38,41},
+                new[]{25,66},
+                new[]{29,47},
+                new[]{4,65},
+                new[]{44,59},
+                new[]{40,89},
+                new[]{7,86},
+                new[]{26,53},
+                new[]{39,81},
+                new[]{6,8},
+                new[]{3,68},
+                new[]{88,91},
+                new[]{42,71},
+                new[]{8,67},
+                new[]{34,89},
+                new[]{5,53},
+                new[]{76,79},
+                new[]{16,75},
+                new[]{44,70},
+                new[]{37,44},
+                new[]{62,94},
+                new[]{66,83},
+                new[]{42,70},
+                new[]{3,76},
+                new[]{22,37},
+                new[]{27,36},
+                new[]{81,96},
+                new[]{11,25},
+                new[]{29,58},
+                new[]{33,81},
+                new[]{36,44}
             }));
         }
     }
