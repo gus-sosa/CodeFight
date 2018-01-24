b40d448da75b26786b82c8a1bb97d4c2d305ff66
Gustavo Sosa <g.sosacuervo1@gmail.com>
2018-01-24 11:24:14 -0600

    Creation of project
    https://codefights.com/interview-practice/task/iXJRYae6TBqc4ymFg

---
+++ longestPath/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace longestPath
+{
+    class Program
+    {
+        int longestPath(string fileSystem)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
