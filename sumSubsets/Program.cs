b23f1033178635731e22362e363b85d22c4414af
Gustavo Sosa <g.sosacuervo1@gmail.com>
2018-01-30 16:35:04 -0600

    Solution to problem sumSubsets
    https://codefights.com/interview-practice/task/uJvihuafm7PRDPCsL/description

--- sumSubsets/Program.cs
+++ sumSubsets/Program.cs
@@ -10,12 +10,47 @@ namespace sumSubsets
     {
         int[][] sumSubsets(int[] arr, int num)
         {
+            var l = new List<int>();
+            var r = new List<int[]>();
+            sumSubsets1(arr, 0, num, l, r);
+            return r.ToArray();
+        }
 
+        private void sumSubsets1(int[] arr, int currentPos, int num, List<int> values, List<int[]> result)
+        {
+            if (num == 0)
+            {
+                var newVal = values.ToArray();
+                bool flag = true;
+                foreach (int[] otherValues in result)
+                    if (newVal.Length == otherValues.Length && Enumerable.Zip(newVal, otherValues, (a, b) => a - b).All(i => i == 0))
+                    {
+                        flag = false;
+                        break;
+                    }
+                if (flag)
+                    result.Add(newVal);
+            }
+            else
+            {
+                if (currentPos == arr.Length) return;
+
+                if (arr[currentPos] <= num)
+                {
+                    values.Add(arr[currentPos]);
+                    sumSubsets1(arr, currentPos + 1, num - arr[currentPos], values, result);
+                    values.RemoveAt(values.Count - 1);
+                }
+
+                sumSubsets1(arr, currentPos + 1, num, values, result);
+            }
         }
 
 
         static void Main(string[] args)
         {
+            var p = new Program();
+            Console.WriteLine(p.sumSubsets(new[] { 1, 2, 3, 4, 5 }, 5));
         }
     }
 }
