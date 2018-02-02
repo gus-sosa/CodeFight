2bf06013bd6540116e237cdfb93e5069edc7f9f5
Gustavo Sosa <g.sosacuervo1@gmail.com>
2018-02-01 23:07:52 -0600

    Solution to problem climbingStairs
    https://codefights.com/interview-practice/task/oJXTWuwEZiC6FTw3A/description

---
+++ climbingStairs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace climbingStairs
+{
+    class Program
+    {
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        int climbingStairs(int n)
+        {
+            if (dict.ContainsKey(n))
+                return dict[n];
+            if (n == 1)
+                return 1;
+            if (n == 2)
+                return 2;
+            return dict[n] = climbingStairs(n - 1) + climbingStairs(n - 2);
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
