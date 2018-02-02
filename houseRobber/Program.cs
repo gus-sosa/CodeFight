aeda320d303143533d97b9a4c16d129b462bbc4e
Gustavo Sosa <g.sosacuervo1@gmail.com>
2018-02-02 12:03:50 -0600

    Solution to problem houseRobber
    https://codefights.com/interview-practice/task/XBWN6DYRqPrKdMZs8/description

---
+++ houseRobber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace houseRobber
+{
+    class Program
+    {
+        int houseRobber(int[] nums)
+        {
+            if (nums.Length == 0)
+                return 0;
+
+            int previousBestEndings = nums[0], previousBestNonEndings = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                int tmp = previousBestNonEndings;
+                previousBestNonEndings = Math.Max(previousBestNonEndings, previousBestEndings);
+                previousBestEndings = tmp + nums[i];
+            }
+
+            return Math.Max(previousBestNonEndings, previousBestEndings);
+        }
+
+
+        static void Main(string[] args)
+        {
+            var p = new Program();
+            Console.WriteLine(p.houseRobber(new[] { 2, 1 }));
+        }
+    }
+}
