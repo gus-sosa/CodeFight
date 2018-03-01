3cf213c58be4d041f6b032cd2522a02b845054cf
Gustavo Sosa <g.sosacuervo1@gmail.com>
2018-03-01 11:08:48 -0600

    Final and accepted solution to the problem

--- mapDecoding/Program.cs
+++ mapDecoding/Program.cs
@@ -9,39 +9,43 @@ namespace mapDecoding
 {
     class Program
     {
+
+        Dictionary<int, int> store = new Dictionary<int, int>();
+        private int mod = Convert.ToInt32(Math.Pow(10, 9) + 7);
+        private string _message;
         int mapDecoding(string message)
         {
-            var counter = new ulong[message.Length + 1];
-            counter[counter.Length - 1] = 1;
-            ulong modulo = (ulong)Math.Pow(10, 12);
-
-            for (int i = message.Length - 1; i >= 0; i--)
-                if (message[i] != '0')
-                {
-                    if (i + 1 < message.Length)
-                    {
-                        counter[i] += counter[i + 1];
-                        int num = Decode(message[i]) * 10 + Decode(message[i + 1]);
-                        if (num < 27)
-                        {
-                            if (i + 2 < message.Length) counter[i] += counter[i + 2];
-                            else counter[i] += 1;
-                        }
-                    }
-                    else counter[i] += 1;
-
-                    counter[i] %= modulo;
-                }
-
-            return (int)(counter[0] % ((ulong)Math.Pow(10, 9) + 7));
+            _message = message;
+            return mapdDecodingRec(message.Length);
         }
 
-        private int Decode(char c) { return c - '0'; }
+        private int mapdDecodingRec(int length)
+        {
+            if (length <= 0)
+                return 1;
+
+            if (store.ContainsKey(length))
+                return store[length];
+
+            if (_message[_message.Length - length] == '0')
+                return 0;
+
+            int result = mapdDecodingRec(length - 1);
+            if (length >= 2)
+            {
+                int num = Convert.ToInt32(_message[_message.Length - length] - '0') * 10 + Convert.ToInt32(_message[_message.Length - length + 1] - '0');
+                if (num <= 26)
+                    result += mapdDecodingRec(length - 2);
+            }
+
+            return store[length] = result % mod;
+        }
 
         static void Main(string[] args)
         {
             var p = new Program();
-            Console.WriteLine(p.mapDecoding("1221112111122221211221221212212212111221222212122221222112122212121212221212122221211112212212211211"));
+            Console.WriteLine(p.mapDecoding("123"));
         }
     }
 }
+
