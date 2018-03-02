cfe3326ffce2b72e5432cccd63040f1478b7d564
Gustavo Sosa <g.sosacuervo1@gmail.com>
2018-03-02 17:41:56 -0600

    Partial solution to the problem
    https://codefights.com/interview-practice/task/gxzW6jpXXkr3JNwXy/description

---
+++ productExceptSelf/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace productExceptSelf
+{
+    class Program
+    {
+        int productExceptSelf(int[] nums, int m)
+        {
+            int product = nums.Aggregate(1, (acc, v) => v * acc);
+            IEnumerable<int> productArray = nums.Select(i => (product / i) % m);
+            return productArray.Sum() % m;
+        }
+
+        static void Main(string[] args)
+        {
+            var p = new Program();
+            Console.WriteLine(p.productExceptSelf(new int[] { 3, 3, 9, 5, 5, 4, 2, 8, 5, 9 }, 17));
+        }
+    }
+}
