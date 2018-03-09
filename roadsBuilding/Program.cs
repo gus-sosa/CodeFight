b9771755d6b04d41d841825c9005afe808d8a310
Gustavo Sosa <g.sosacuervo1@gmail.com>
2018-03-09 14:55:22 -0600

    Creating new project
    https://codefights.com/arcade/graphs-arcade/kingdom-roads/CSzczQWdnYwmyEjvv

---
+++ roadsBuilding/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace roadsBuilding
+{
+    class Program
+    {
+        int[][] roadsBuilding(int cities, int[][] roads)
+        {
+
+        }
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
