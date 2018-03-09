39a4409cafb789597edbf9dfb4583e4f275639ad
Gustavo Sosa <g.sosacuervo1@gmail.com>
2018-03-09 10:46:43 -0600

    Solution to problem sumOfTwo
    https://codefights.com/interview-practice/task/Hm98RnqK9Be575yoj

---
+++ sumOfTwo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sumOfTwo
+{
+    class Program
+    {
+        bool sumOfTwo(int[] a, int[] b, int v)
+        {
+            var dic = new Hashtable();
+            foreach (var i in a)
+            {
+                var term = v - i;
+                if (!dic.ContainsKey(term))
+                    dic.Add(term, 1);
+            }
+
+            foreach (var i in b)
+            {
+                if (dic.ContainsKey(i))
+                    return true;
+            }
+
+            return false;
+        }
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
