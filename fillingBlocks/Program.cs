cab1270bb727f491c09105b4440099e4d705f239
Gustavo Sosa <g.sosacuervo1@gmail.com>
2018-03-09 12:06:01 -0600

    Solution to the problem fillingBlocks
    https://codefights.com/interview-practice/task/yWF4MmhvtmzfKNWgt

---
+++ fillingBlocks/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fillingBlocks
+{
+    class Program
+    {
+        Dictionary<int, int> store = new Dictionary<int, int>()
+        {
+            {2,5},
+            {3,11 }
+        };
+
+        int fillingBlocks(int n) { return n <= 1 ? 1 : (store.ContainsKey(n) ? store[n] : store[n] = fillingBlocks(n - 1) + 5 * fillingBlocks(n - 2) + fillingBlocks(n - 3) - fillingBlocks(n - 4)); }
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
