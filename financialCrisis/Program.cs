90b359f7236153dc5c9d42103eba3cd2230e2291
Gustavo Sosa <g.sosacuervo1@gmail.com>
2018-03-12 14:04:49 -0500

    Creation of project financialCrisis
    https://codefights.com/arcade/graphs-arcade/kingdom-roads/wkhfpfiT6YynLk2qE

---
+++ financialCrisis/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace financialCrisis
+{
+    class Program
+    {
+        bool[][][] financialCrisis(bool[][] roadRegister)
+        {
+
+        }
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
