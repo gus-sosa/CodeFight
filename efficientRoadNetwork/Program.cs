15bbc551bb0dce1b4ae200e764f3748d8f7d089b
Gustavo Sosa <g.sosacuervo1@gmail.com>
2018-03-12 11:33:52 -0500

    Creation of project
    https://codefights.com/arcade/graphs-arcade/kingdom-roads/ty4w8WJZ4sZSBNK5Q

---
+++ efficientRoadNetwork/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace efficientRoadNetwork
+{
+    class Program
+    {
+        bool efficientRoadNetwork(int n, int[][] roads)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
