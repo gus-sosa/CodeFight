1ffd725308f842df0125056bda1c3bc06ae4c26b
Gustavo Sosa <g.sosacuervo1@gmail.com>
2018-03-12 11:10:54 -0500

    Solution to problem

--- roadsBuilding/Program.cs
+++ roadsBuilding/Program.cs
@@ -10,7 +10,17 @@ namespace roadsBuilding
     {
         int[][] roadsBuilding(int cities, int[][] roads)
         {
+            var matrix = new bool[cities, cities];
+            foreach (int[] road in roads)
+                matrix[road[0], road[1]] = matrix[road[1], road[0]] = true;
 
+            var result = new List<int[]>();
+            for (int i = 0; i < cities; i++)
+                for (int j = i + 1; j < cities; j++)
+                    if (!matrix[i, j])
+                        result.Add(new int[] { i, j });
+
+            return result.ToArray();
         }
 
 
