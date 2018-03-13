68259359801685c4488ee626557cf12fff7b8d51
Gustavo Sosa <g.sosacuervo1@gmail.com>
2018-03-13 10:02:53 -0500

    Solution to problem

--- NamingRoads/Program.cs
+++ NamingRoads/Program.cs
@@ -10,7 +10,28 @@ namespace NamingRoads
     {
         bool namingRoads(int[][] roads)
         {
+            var dict = new Dictionary<int, SortedSet<int>>();
 
+            foreach (int[] road in roads)
+            {
+                int city1 = road[0], city2 = road[1], nameRoad = road[2];
+                if (!dict.ContainsKey(city1))
+                    dict[city1] = new SortedSet<int>();
+                if (!dict.ContainsKey(city2))
+                    dict[city2] = new SortedSet<int>();
+                dict[city1].Add(nameRoad);
+                dict[city2].Add(nameRoad);
+            }
+
+            foreach (int dictKey in dict.Keys)
+            {
+                var names = dict[dictKey].ToArray();
+                for (int i = 1; i < names.Length; i++)
+                    if (names[i] - names[i - 1] == 1)
+                        return false;
+            }
+
+            return true;
         }
 
         static void Main(string[] args)
